2e81572cd72852410bfa15cf690cdbb663714d12
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-02-24 20:03:20 -0600

    Implementando el método de la búsqueda binaria

--- src/Busquedas.java
+++ src/Busquedas.java
@@ -8,6 +8,21 @@ public class Busquedas {
         return -1; // si el valor no se encuentra en el arreglo, retorna -1
     }
 
+    public static int busquedaBinaria(int[] arreglo, int valorBuscado) {
+        int inicio = 0;
+        int fin = arreglo.length - 1;
+        while (inicio <= fin) {
+            int medio = (inicio + fin) / 2;
+            if (arreglo[medio] == valorBuscado) {
+                return medio; // retorna el índice donde se encuentra el valor buscado
+            } else if (arreglo[medio] < valorBuscado) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
+        }
+        return -1; // si el valor no se encuentra en el arreglo, retorna -1
+    }
 
     public static void main(String[] args) {
 
