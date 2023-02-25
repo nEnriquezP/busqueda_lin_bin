958865ae656cac0eebc4f837cbed0a92c9efbfe4
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-02-24 19:59:23 -0600

    Implementando código de búsqueda lineal

--- src/Busquedas.java
+++ src/Busquedas.java
@@ -1,4 +1,14 @@
 public class Busquedas {
+    public static int busquedaLineal(int[] arreglo, int valorBuscado) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valorBuscado) {
+                return i; // retorna el índice donde se encuentra el valor buscado
+            }
+        }
+        return -1; // si el valor no se encuentra en el arreglo, retorna -1
+    }
+
+
     public static void main(String[] args) {
 
     }
